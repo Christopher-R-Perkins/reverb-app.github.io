d0250832cdb9968cb260d4b0483a4c9ea0dcb6b9
Morgan Davis <mdavisillustration@gmail.com>
2024-04-26 00:02:17 -0400

    feat: overhauled styling and updated diagrams

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./javascripts/script.js"],
+  content: ['./*.html', './javascripts/script.js'],
   theme: {
-    colors: {
-      black: "#041a1c",
-      dark: "#003C43",
-      dim: "#135D66",
-      bright: "#77B0AA",
-      light: "#E3FEF7",
-    },
     extend: {
       keyframes: {
         slideIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
         },
       },
       animation: {
-        slideIn: "slideIn .25s ease-in-out forwards 1s",
+        slideIn: 'slideIn .25s ease-in-out forwards 1s',
+      },
+      colors: {
+        bgBlue: '#25384D',
+        offWhite: '#FAFFFA',
+        turquoise: {
+          100: '#FAFFFA',
+          200: '#CEFFF9',
+          300: '#B0FAF1',
+          400: '#97FFE9',
+          500: '#7EFFE2',
+          600: '#31F8DA',
+          700: '#00E9CA',
+          800: '#00b099',
+        },
+        black: '#041a1c',
+        dark: '#111827',
+        dim: '#135D66',
+        bright: '#77B0AA',
+        light: '#E3FEF7',
+        bgGray: '#1f2937',
+      },
+      fontFamily: {
+        varela: ['Varela Round', 'sans-serif'],
       },
     },
   },
